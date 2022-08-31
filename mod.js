7d6a61a2ddb4f53ac81eb5cb0b9c8e3eb0e9ef33
Jesper van den Ende <jespertheend@gmail.com>
2022-08-31 15:16:33 +0200

    Add getScriptDir()

--- mod.js
+++ mod.js
@@ -1,7 +1,7 @@
 import {
   dirname,
   fromFileUrl,
-} from "https://deno.land/std@0.119.0/path/mod.ts";
+} from "https://deno.land/std@0.153.0/path/mod.ts";
 
 /**
  * Changes the current working directory to the folder that contains the main
@@ -10,6 +10,14 @@ import {
  * to change to a directory to that of a script that is not the main entry point.
  */
 export function setCwd(importMeta = Deno.mainModule) {
-  const scriptPath = dirname(fromFileUrl(importMeta));
-  Deno.chdir(scriptPath);
+  Deno.chdir(getScriptDir(importMeta));
+}
+
+/**
+ * Gets the path to the directory that contains the main module of your script.
+ * @param {string} importMeta Optionally provide an import.meta.url if you want
+ * to change to a directory to that of a script that is not the main entry point.
+ */
+export function getScriptDir(importMeta = Deno.mainModule) {
+  return dirname(fromFileUrl(importMeta));
 }
